6cdc4901c70520ca38e43521bd1bdbca7169e88e
kingjeongkong <wjdqls9223@gmail.com>
2024-12-04 22:22:46 +0900

    Singly Linked List Deletion, Traverse, Searching

--- DataStructure/linkedLists.py
+++ DataStructure/linkedLists.py
@@ -37,3 +37,62 @@ class SinglyLinkedList:
     new_node = Node(data)
     new_node.next = current.next
     current.next = new_node
+
+  def delete_node(self, data):
+    temp = self.head
+
+    if temp and temp.data == data:
+      self.head = temp.next
+      temp = None
+      return
+
+    prev = None
+    while temp and temp.data != data:
+      prev = temp
+      temp = temp.next
+
+    if not temp:
+      print(f"Node with data {data} not found.")
+      return
+    
+    prev.next = temp.next
+    temp = None
+
+  def delete_at_position(self, position):
+    temp = self.head
+
+    if not temp:
+      return
+    
+    if position == 0:
+      self.head = temp.next
+      return
+    
+    for i in range(position - 1):
+      temp = temp.next
+      if not temp:
+        break
+
+    if not temp or temp.next:
+      print("Position is out of bounds.")
+      return
+    
+    temp.next = temp.next.next
+    temp.next = None
+
+  def traverse(self):
+    current = self.head
+    while current:
+      print(current.data, end=' -> ')
+      current = current.next
+    print('None')
+
+  def searching(self, data):
+    current = self.head
+    while current:
+      if current.data == data:
+        return True
+      current = current.next
+    return False
+      
+    
