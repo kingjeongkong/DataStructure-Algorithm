d2cd2fe4fe12eb0c609e6aa8a599bb99d7c9ed18
kingjeongkong <wjdqls9223@gmail.com>
2024-12-24 00:44:11 +0900

    Linked List Queue

--- DataStructure/Queue.py
+++ DataStructure/Queue.py
@@ -47,3 +47,60 @@ class ArrayQueue:
             i = (i + 1) % self.capacity
             count += 1
         print()
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedListQueue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        self.size = 0
+
+    def is_empty(self):
+        return self.front is None
+
+    def enqueue(self, data):
+        new_node = Node(data)
+        if self.rear is None:
+            self.front = self.rear = new_node
+        else:
+            self.rear.next = new_node
+            self.rear = new_node
+        self.size += 1
+
+    def dequeue(self):
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+
+        data = self.front.data
+        self.front = self.front.next
+        self.size -= 1
+
+        if self.front is None:
+            self.rear = None
+
+        return data
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+        return self.front.data
+
+    def get_size(self):
+        return self.size
+
+    def display(self):
+        if self.is_empty():
+            print("Queue is empty")
+            return
+
+        print("Queue (front -> rear):", end=" ")
+        current = self.front
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+
