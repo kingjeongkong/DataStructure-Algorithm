680bc472f24dd0005e0fbf82334f6a4451b36993
kingjeongkong <wjdqls9223@gmail.com>
2024-12-27 21:39:15 +0900

    K번째 수 (다른 풀이)

--- Algorithms/정렬/K번째 수.py
+++ Algorithms/정렬/K번째 수.py
@@ -10,3 +10,15 @@ def solution(array, commands):
         answer.append(sorted_array[i][commands[i][2] - 1])
     
     return answer
+    
+
+def solution(array, commands):
+    answer = []
+    
+    for command in commands:
+        i, j, k = command
+        sliced_array = array[i-1:j]
+        sorted_array = sorted(sliced_array)
+        answer.append(sorted_array[k-1])
+    
+    return answer
